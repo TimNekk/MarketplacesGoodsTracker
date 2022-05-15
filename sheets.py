f3d21aa5fc1ca798769d06713086ea9b851f3fb5
Tim <herew26@gmail.com>
2022-05-16 02:19:48 +0300

    history added

--- sheets.py
+++ sheets.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 
 import gspread
@@ -11,8 +12,10 @@ class Sheets:
         self.sheet = self.workbook.sheet1
 
     def get_urls(self) -> List[str]:
-        return self.sheet.col_values(2)
+        return self.sheet.col_values(1)[1:]
 
     def set_quantities(self, quantities: List[int]):
-        cells = tuple(map(lambda x: ["Нет в наличии"] if x == -1 else [x], quantities))
-        self.sheet.update("A1", cells)
+        cells = list(map(lambda x: "Нет в наличии" if x == -1 else x, quantities))
+        cells.insert(0, datetime.now().strftime("%d/%m"))
+        print(cells)
+        self.sheet.insert_cols([cells], col=3)
