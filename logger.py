2226c63db35531b7ebee5fd9957d9f5cbd6d1982
TimNekk <herew26@gmail.com>
2022-07-06 12:25:21 +0300

    logger added

---
+++ logger.py
@@ -0,0 +1,28 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+import coloredlogs
+
+
+def _check_and_create_logging_directory(path):
+    import os
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+fmt = "%(asctime)s [%(levelname)s] - (%(filename)s).%(funcName)s(%(lineno)d) > %(message)s"
+log_directory = "logs/"
+log_file = "log.log"
+file_handler_suffix = "%Y-%m-%d"
+
+_check_and_create_logging_directory(log_directory)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+coloredlogs.install(level="INFO", logger=logger, fmt=fmt)
+
+file_handler = TimedRotatingFileHandler(log_directory + log_file, when="midnight", interval=1)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(fmt))
+
+logger.addHandler(file_handler)
